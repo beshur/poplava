03e94159610e6f3e271aa5ecbf8c4b32594eb988
Alex Buznik <shu@buznik.net>
2024-07-30 12:31:36 +0200

    update poplava 125 & 20

---
+++ tools/fetchMetadataForId.js
@@ -0,0 +1,69 @@
+// const fetch = require('node-fetch');
+
+const API_KEY = 'AIzaSyC2bmo58bCm4NEwCuB7L1Z_nDikQd-Fi0w';
+// const VIDEO_ID = 'ExHqD3o26u4';
+// const VIDEO_ID = process.argv[2];
+
+const fetchYoutubeMetadata = async (videoId) => {
+  const url = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=snippet,contentDetails,statistics&key=${API_KEY}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    if (data.items.length === 0) {
+      throw new Error('Video not found');
+    }
+    return data.items[0];
+  } catch (error) {
+    console.error('Error fetching video metadata:', error);
+    return null;
+  }
+};
+
+const extractTimestampsWithTitles = (description) => {
+  const lines = description.split('\n');
+  const timestampRegex = /\b\d{1,2}:\d{2}\b/;
+  const timestampsWithTitles = {};
+
+  lines.forEach(line => {
+    const match = line.match(timestampRegex);
+    if (match) {
+      const timestamp = match[0];
+      const title = line.replace(new RegExp(timestamp + ' —|- '), '').trim();
+      timestampsWithTitles[timestamp] = title;
+    }
+  });
+
+  return timestampsWithTitles;
+};
+async function fetchMetadataForId(VIDEO_ID) {
+  const metadata = await fetchYoutubeMetadata(VIDEO_ID)
+  if (metadata) {
+    const date = new Date(metadata.snippet.publishedAt);
+    const dateFormatted = `${date.getFullYear()}-${date.getMonth() + 1 < 10 ? '0' : ''}${date.getMonth() + 1}-${date.getDate()}`;
+    const title = metadata.snippet.localized.title;
+    const num = metadata.snippet.localized.title.match(/(\d+)/)[1];
+    const timestamps = extractTimestampsWithTitles(metadata.snippet.description);
+    console.log('Extracted Timestamps:', timestamps);
+    console.log('Video Metadata:', {
+      dateFormatted,
+      title,
+      num
+    });
+    return {
+      id: VIDEO_ID,
+      date: dateFormatted,
+      title,
+      num,
+      timestamps
+    }
+  }
+  return null;
+}
+
+module.exports = {
+  fetchMetadataForId
+}
---
+++ tools/storeVideoMetadata.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+const { fetchMetadataForId } = require('./fetchMetadataForId');
+
+const videoId = process.argv[2];
+
+async function storeVideoMetadata() {
+  const filePath = path.join(__dirname, '../data.json');
+  if (fs.existsSync(filePath)) {
+    const newEntry = await fetchMetadataForId(videoId);
+
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const fileJson = JSON.parse(fileContent);
+    let result = '';
+    if (fileJson[videoId]) {
+      fileJson[videoId] = newEntry;
+      result = JSON.stringify(fileJson, null, 2);
+    } else {
+      const addition = `"${videoId}":${JSON.stringify(newEntry)},`;
+      console.log('New entry:', addition);
+      result = fileContent.split('\n')
+      result.splice(1, 1, addition);
+      result = result.join('\n');
+    }
+    fs.writeFileSync(filePath, result, 'utf-8');
+    console.log('Data updated successfully');
+  } else {
+    throw new Error('Data file not found');
+  }
+}
+storeVideoMetadata();

5d64b7d6b5f115619f24ea4a60421ba2c2f79ba2
Alex Buznik <shu@buznik.net>
2024-08-09 10:59:45 +0200

    Fix json parsing and storing

--- tools/storeVideoMetadata.js
+++ tools/storeVideoMetadata.js
@@ -6,28 +6,33 @@ const { fetchMetadataForId } = require('./fetchMetadataForId');
 const videoId = process.argv[2];
 
 async function storeVideoMetadata() {
-  const filePath = path.join(__dirname, '../data.json');
-  if (fs.existsSync(filePath)) {
-    const newEntry = await fetchMetadataForId(videoId);
-    if (!newEntry) {
-      return;
-    }
-    const fileContent = fs.readFileSync(filePath, 'utf-8');
-    const fileJson = JSON.parse(fileContent);
-    let result = '';
-    if (typeof fileJson[videoId] !== 'undefined') {
-      fileJson[videoId] = newEntry;
-      result = JSON.stringify(fileJson, null, 2);
+  try {
+    const filePath = path.join(__dirname, '../data.json');
+    if (fs.existsSync(filePath)) {
+      const newEntry = await fetchMetadataForId(videoId);
+      if (!newEntry) {
+        return;
+      }
+      const fileContent = fs.readFileSync(filePath, 'utf-8');
+      const fileJson = JSON.parse(fileContent);
+      let result = '';
+      if (typeof fileJson[videoId] !== 'undefined') {
+        fileJson[videoId] = newEntry;
+        result = JSON.stringify(fileJson, null, 2);
+      } else {
+        const addition = `"${videoId}":${JSON.stringify(newEntry)},`;
+        result = fileContent.split('\n')
+        result.splice(1, 0, addition);
+        result = result.join('\n');
+      }
+      fs.writeFileSync(filePath, result, 'utf-8');
+      console.log('Data updated successfully');
     } else {
-      const addition = `"${videoId}":${JSON.stringify(newEntry)},`;
-      result = fileContent.split('\n')
-      result.splice(1, 1, addition);
-      result = result.join('\n');
+      throw new Error('Data file not found');
     }
-    fs.writeFileSync(filePath, result, 'utf-8');
-    console.log('Data updated successfully');
-  } else {
-    throw new Error('Data file not found');
+  } catch (error) {
+    console.error('Error storing video metadata:', error);
+    process.exit(1);
   }
 }
 storeVideoMetadata();
